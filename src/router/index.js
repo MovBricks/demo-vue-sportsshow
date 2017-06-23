ab609721e77ecf6b2dd22e56da9d6ef319ae3be7
Acer <youngbird92@gmail.com>
2017-06-23 18:45:51 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -11,79 +11,99 @@ import pageheadTitle from '../pages/pageHeadTitle.vue'
 import pageSportsSteps from '../pages/pageSportsSteps.vue'
 import sportsWeight from '../pages/pageSportsWeight.vue'
 import pageDraggable from '../pages/pageDraggable.vue'
+import pageSideBar from '../components/componentSideBar.vue'
 import pageMain from '../pages/pageMain.vue'
 import NotFoundComponent from '../components/Hello.vue'
-import pageSideBar from '../components/componentSideBar.vue'
 
 Vue.use(Router)
 
+const devRoutesArray = [
+  {
+    path: '/',
+    name: 'index',
+    component: index
+  },
+  {
+    path: '/btn',
+    name: 'btn',
+    component: pageQuiButton
+  },
+  {
+    path: '/list',
+    name: 'list',
+    component: pageQuiList
+  },
+  {
+    path: '/nav',
+    name: 'nav',
+    component: pageQuiNav
+  },
+  {
+    path: '/show',
+    name: 'show',
+    component: pageEchartsTest
+  },
+  {
+    path: '/sports',
+    name: 'sports',
+    component: pageSports
+  },
+  {
+    path: '/headtitle',
+    name: 'headTitle',
+    component: pageheadTitle
+  },
+  {
+    path: '/sportsSteps',
+    name: 'sportsSteps',
+    component: pageSportsSteps
+  },
+  {
+    path: '/sportsWeight',
+    name: 'sportsWeight',
+    component: sportsWeight
+  },
+  {
+    path: '/pageDraggable',
+    name: 'pageDraggable',
+    component: pageDraggable
+  },
+  {
+    path: '/pageSideBar',
+    name: 'pageSideBar',
+    component: pageSideBar
+  },
+  {
+    path: '/pageMain',
+    name: 'pageMain',
+    component: pageMain
+  },
+  {
+    path: '*',
+    component: NotFoundComponent
+  }
+]
+
+const buildRoutesArray = [
+  {
+    path: '/',
+    name: 'index',
+    component: index
+  },
+  {
+    path: '/pageMain',
+    name: 'pageMain',
+    component: pageMain
+  },
+  {
+    path: '*',
+    component: NotFoundComponent
+  }
+]
+
 export default new Router({
   // mode: 'history',
-  routes: [
-
-    {
-      path: '/',
-      name: 'index',
-      component: index
-    },
-    {
-      path: '/btn',
-      name: 'btn',
-      component: pageQuiButton
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: pageQuiList
-    },
-    {
-      path: '/nav',
-      name: 'nav',
-      component: pageQuiNav
-    },
-    {
-      path: '/show',
-      name: 'show',
-      component: pageEchartsTest
-    },
-    {
-      path: '/sports',
-      name: 'sports',
-      component: pageSports
-    },
-    {
-      path: '/headtitle',
-      name: 'headTitle',
-      component: pageheadTitle
-    },
-    {
-      path: '/sportsSteps',
-      name: 'sportsSteps',
-      component: pageSportsSteps
-    },
-    {
-      path: '/sportsWeight',
-      name: 'sportsWeight',
-      component: sportsWeight
-    },
-    {
-      path: '/pageDraggable',
-      name: 'pageDraggable',
-      component: pageDraggable
-    },
-    {
-      path: '/pageSideBar',
-      name: 'pageSideBar',
-      component: pageSideBar
-    },
-    {
-      path: '/pageMain',
-      name: 'pageMain',
-      component: pageMain
-    },
-    {
-      path: '*',
-      component: NotFoundComponent
-    }
-  ]
+  routes: process.env.NODE_ENV === 'production'
+    ? buildRoutesArray
+    : devRoutesArray
 })
